a35a00275d499085f8788d2fbe350d30008e6e13
Your Name <you@example.com>
2022-04-27 16:17:29 -0500

    evidencia paises sin acabar

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,63 @@ Route::get('/', function () {
 Route::get('hola', function(){
     echo "Hola le dijo la araña a la mosca";
 });
+
+Route::get('paises', function(){
+   $paises =[
+       "Colombia" => [
+           "cap" => "Bogotá",
+           "mon" => "Peso",
+           "pob" => 51.6,
+           "ciudades" => [
+               "Medellin",
+               "Cali",
+               "Barranquilla"
+           ]
+           ],
+       "Peru" => [
+        "cap" => "Lima",
+        "mon" => "Sol",
+        "pob" => 32.9,
+        "ciudades" => [
+            "Cusco",
+            "Trujillo"
+        ]
+        ],
+        "Chile" => [
+            "cap" => "Santiago de Chile",
+            "mon" => "Peso",
+            "pob" => 12.1,
+            "ciudades" => [
+                "Iquique",
+                "Concepción",
+                "Valparaíso",
+                "Temuco"
+
+            ]
+            ],
+            "Argentina" => [
+                "cap" => "Buenos Aires",
+                "mon" => "Peso",
+                "pob" => 45.3,
+                "ciudades" => [
+                    "Rosario",
+                    "Córdoba",
+                    "Mar del Plata"
+                ]
+                ],
+                "Mexico" => [
+                    "cap" => "Ciudad de México",
+                    "mon" => "Peso",
+                    "pob" => 128.9,
+                    "ciudades" => [
+                        "Guadalajara",
+                        "Cancún",
+                        "Guanajuato",
+                        "Monterrey",
+                        "Mexicali"
+                    ]
+                ]
+   ] ;
+   return view('paises')
+            ->with("paises", $paises);
+});
